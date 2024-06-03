54b68cba50568732f7528559d742382a5228713d
Lilia <liliaribeirosousa@gmail.com>
2024-06-03 14:20:09 +0200

    moved icons to src

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -83,11 +83,11 @@ const PostList = () => {
           </div>
           <p className="timestamp">Posted on: {new Date(blog.timestamp).toLocaleString()}</p>
           <div className="recipeRating">
-            <img src={blog.rating >= 1 ? '../icons/icons8-star-50(1).png' : '../icons/icons8-star-50.png'} alt="star" className="star" />
-            <img src={blog.rating >= 2 ? '../icons/icons8-star-50(1).png' : '../icons/icons8-star-50.png'} alt="star" className="star" />
-            <img src={blog.rating >= 3 ? '../icons/icons8-star-50(1).png' : '../icons/icons8-star-50.png'} alt="star" className="star" />
-            <img src={blog.rating >= 4 ? '../icons/icons8-star-50(1).png' : '../icons/icons8-star-50.png'} alt="star" className="star" />
-            <img src={blog.rating == 5 ? '../icons/icons8-star-50(1).png' : '../icons/icons8-star-50.png'} alt="star" className="star" />
+            <img src={blog.rating >= 1 ? 'src/icons/icons8-star-50(1).png' : 'src/icons/icons8-star-50.png'} alt="star" className="star" />
+            <img src={blog.rating >= 2 ? 'src/icons/icons8-star-50(1).png' : 'src/icons/icons8-star-50.png'} alt="star" className="star" />
+            <img src={blog.rating >= 3 ? 'src/icons/icons8-star-50(1).png' : 'src/icons/icons8-star-50.png'} alt="star" className="star" />
+            <img src={blog.rating >= 4 ? 'src/icons/icons8-star-50(1).png' : 'src/icons/icons8-star-50.png'} alt="star" className="star" />
+            <img src={blog.rating == 5 ? 'src/icons/icons8-star-50(1).png' : 'src/icons/icons8-star-50.png'} alt="star" className="star" />
           </div>
           <Link to={`/blogs/${blog.id}`} className="read-more-link">Read More</Link>
         </div>
